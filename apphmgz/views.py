caa4c05f2818fbfc83a58f68244503a5dcb80108
Harol Mauricio Gómez Zapata <harolmgzapata@gmail.com>
2025-08-14 23:46:38 -0500

    cuarto commit

--- apphmgz/views.py
+++ apphmgz/views.py
@@ -5,6 +5,11 @@ from .models import Usuarios
 # Create your views here.
 
 def index(request):
+    usuario = Usuarios()
+    usuario.usuario = "harol.gomez"
+    usuario.correo = "hmgomezz@sena.edu.co"
+    usuario.clave = "123456"
+    usuario.save()
     usuarios = Usuarios.objects.all()
     context = {'usuarios': usuarios}
     return render(request, 'render/index.html', {})
